113d556e4788ac4931cfa3272771c6e3cbf555ce
bliid <gh.bliid@gmail.com>
2019-09-11 15:42:19 +0100

    Updates

---
+++ precache-manifest.c065dc5285e6f0250bf40db79602c3f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd175237de2e87d2f7cb1da711476516",
+    "url": "/docplane/index.html"
+  },
+  {
+    "revision": "58f37b8b946e42d68a45",
+    "url": "/docplane/static/css/main.8aeb61b5.chunk.css"
+  },
+  {
+    "revision": "0d93b52866cee7040d9c",
+    "url": "/docplane/static/js/2.6bd3bc96.chunk.js"
+  },
+  {
+    "revision": "58f37b8b946e42d68a45",
+    "url": "/docplane/static/js/main.36c618a8.chunk.js"
+  },
+  {
+    "revision": "65b979a02388024527d4",
+    "url": "/docplane/static/js/runtime~main.e181b236.js"
+  },
+  {
+    "revision": "8e5b345ff9df62455087a329d3b48498",
+    "url": "/docplane/static/media/logo.8e5b345f.svg"
+  }
+]);
